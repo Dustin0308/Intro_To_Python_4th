cc1419c2c8671ba9952af34100da76bff6951f44
Dustin0308 <dustinreaver@gmail.com>
2025-11-06 23:00:08 +0000

    Input/Output exercises.

---
+++ input_output/exercise_1.py
@@ -0,0 +1,32 @@
+# Write a program named greeter.py. The program should ask for your name, then output Hello, NAME! 
+# where NAME is the name you entered:
+
+# $ python greeter.py
+# What is your name? Sue
+# Hello, Sue!
+
+
+
+
+
+
+# My Answer (Can also be found in the 'greeter.py' file):
+
+name = input('What is your name? ')
+print(f'Hello, {name}!')                # Hello, Dustin!
+
+
+
+
+
+
+
+# Launch School's Answer:
+
+# Solution 1:
+name = input('What is your name? ')
+print('Hello, ' + name + '!')
+
+# Solution 2:
+name = input('What is your name? ')
+print(f'Hello, {name}!')
